855f46e786c7ef693918cf4f9918d0150e1d56ae
yuxuanchiadm <yuxuanchiadm@126.com>
2018-04-21 06:20:15 +0800

    Add union type

--- src/main/scala/org/monadscala/Typelevel.scala
+++ src/main/scala/org/monadscala/Typelevel.scala
@@ -4,4 +4,6 @@ import scala.language.higherKinds
 
 object Typelevel {
   type Currying[T[_, _], A] = { type Type[B] = T[A, B] }
+
+  type Union[A, B] = { type Type[T] = ((T => Nothing) => Nothing) <:< (((A => Nothing) with (B => Nothing)) => Nothing) }
 }
