77242bb96ca707d67813e476df36621433ee1a7f
yuxuanchiadm <yuxuanchiadm@126.com>
2018-05-01 11:19:40 +0800

    Update type level utils

--- src/main/scala/org/monadscala/Typelevel.scala
+++ src/main/scala/org/monadscala/Typelevel.scala
@@ -3,7 +3,41 @@ package org.monadscala
 import scala.language.higherKinds
 
 object Typelevel {
-  type Currying[T[_, _], A] = { type Type[B] = T[A, B] }
+  type Curry0[F] = {
+    type <| = F
+  }
 
-  type Union[A, B] = { type Type[T] = ((T => Nothing) => Nothing) <:< (((A => Nothing) with (B => Nothing)) => Nothing) }
+  type Curry1[F[_]] = {
+    type <|[A] = F[A]
+    type <[A] = {
+      type <| = F[A]
+    }
+  }
+
+  type Curry2[F[_, _]] = {
+    type <|[A, B] = F[A, B]
+    type <[A] = {
+      type <|[B] = F[A, B]
+      type <[B] = {
+        type <| = F[A, B]
+      }
+    }
+  }
+
+  type Curry3[F[_, _, _]] = {
+    type <|[A, B, C] = F[A, B, C]
+    type <[A] = {
+      type <|[B, C] = F[A, B, C]
+      type <[B] = {
+        type <|[C] = F[A, B, C]
+        type <[C] = {
+          type <| = F[A, B, C]
+        }
+      }
+    }
+  }
+
+  type Not[A] = A => Nothing
+
+  type Union[A, B] = { type Apply[C] = Not[Not[C]] <:< Not[Not[A] with Not[B]] }
 }
---
+++ src/test/scala/org/monadscala/test/TypelevelTest.scala
@@ -0,0 +1,20 @@
+package org.monadscala.test
+
+import scala.language.higherKinds
+
+import org.monadscala.Typelevel._
+import org.scalatest.FunSpec
+
+class TypelevelTest extends FunSpec {
+  describe("Test type level currying") {
+    it("Should compils") {
+      def fun[F]: Unit = ()
+      def foo[F[_]]: Unit = ()
+      def bar[F[_, _]]: Unit = ()
+
+      assertCompiles("fun[Curry2[Either]# <[List[String]]# <[Option[Int]]# <|]")
+      assertCompiles("foo[Curry2[Either]# <[List[String]]# <|]")
+      assertCompiles("bar[Curry2[Either]# <|]")
+    }
+  }
+}
