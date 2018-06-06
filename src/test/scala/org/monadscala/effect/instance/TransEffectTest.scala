e6b4f4977f3708af855265a30d1fa2e9d4524dc4
yuxuanchiadm <yuxuanchiadm@126.com>
2018-06-07 05:43:59 +0800

    Add Cont monad testcase for TransEffect

--- src/test/scala/org/monadscala/effect/instance/TransEffectTest.scala
+++ src/test/scala/org/monadscala/effect/instance/TransEffectTest.scala
@@ -3,7 +3,9 @@ package org.monadscala.effect.instance
 import org.monadscala.Typelevel._
 import org.monadscala.effect.Effects._
 import org.monadscala.effect.instance.TransEffect._
+import org.monadscala.instance._
 import org.monadscala.instance.Option._
+import org.monadscala.instance.Cont._
 import org.scalatest.FunSpec
 
 class TransEffectTest extends FunSpec {
@@ -14,6 +16,12 @@ class TransEffectTest extends FunSpec {
         i <- pure(1)
       } yield i
       assertResult(Option.empty[Int])(runInit(prog, () :: Init.empty))
+    }
+    it("Test Cont monad") {
+      def prog[R](n: Int): SimpleEff[Curry2[Cont]# <[R]# <|, Int, EList# ::[TRANS[Curry2[Cont]# <[R]# <|]]# ::|] = for {
+        i <- promote(callCC[R, Int, Int](k => k(n * n)))
+      } yield i
+      assertResult(16)(performCont(runInit(prog[Int](4), () :: Init.empty)))
     }
   }
 }
